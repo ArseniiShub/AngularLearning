0bc3c049a29a2ad3f0d53c0884cd3ebbdcdb22b1
ArseniiShub <arsenii.shub@sharp-dev.net>
2024-01-10 14:17:42 +0100

    Added Details page content

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HousingLocation } from "../housing-location";
+import { HousingService } from "../housing.service";
 
 @Component({
   selector: 'app-details',
@@ -8,5 +11,13 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  route = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocationId = -1;
+  housingLocation: HousingLocation | undefined;
 
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
 }
