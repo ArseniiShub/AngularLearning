2754dc546e53c2175876b0fa27d507d25e5b66a4
ArseniiShub <arsenii.shub@sharp-dev.net>
2024-01-10 13:45:38 +0100

    Injection and Routing

---
+++ src/app/routes.ts
@@ -0,0 +1,18 @@
+﻿import { Route } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { DetailsComponent } from "./details/details.component";
+
+const routeConfig: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page'
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home details'
+  }
+];
+
+export default routeConfig;
